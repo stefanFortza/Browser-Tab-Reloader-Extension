03f654f5e47a6f9d0c3fa1ccbe0502e004e71752
Tacu Darius Stefan <tacustefan74@gmail.com>
2024-07-16 14:44:25 +0300

    first commit

---
+++ browser-tab-reloader-vscode-extension/src/extension.ts
@@ -0,0 +1,40 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { Server } from "socket.io";
+const io = new Server();
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+io.on("connection", () => {
+  console.log("connection");
+});
+
+vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
+  console.log("Document changed.");
+  io.emit("change");
+  console.log(e.getText());
+});
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "browser-tab-reloader-vscode-extension" is now active!'
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "browser-tab-reloader-vscode-extension.helloWorld",
+    () => {}
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
+
+io.listen(3000);
